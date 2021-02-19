900246a4d387eb99eeee17260787af0c60294581
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-02-20 02:41:35 +0800

    restore old one

--- Gemfile
+++ Gemfile
@@ -7,5 +7,3 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 gem "jekyll", "~> 4.1"
-
-gem "webrick", "~> 1.7"
--- js/creative.js
+++ js/creative.js
@@ -11,7 +11,7 @@
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 75)
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
