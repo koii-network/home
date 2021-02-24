cdca5e708f4b8284e16fd208b315e326da592fec
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-02-24 14:24:05 +0800

    close menu when clicking outside

--- js/util.js
+++ js/util.js
@@ -10,26 +10,31 @@
   $(document.body).scroll(function () {
     var aTop = $("#topbar").height();
     var pathname = window.location.pathname;
-    if (pathname === '/') {
-    if ($(this).scrollTop() >= aTop) {
-      console.log("$(this).scrollTop()", $(this).scrollTop(), aTop);
-      $("#topbar").css({
-        background: "linear-gradient(90.16deg, #030332 0.14%, #171753 99.87%)",
-        boxShadow: "0px 10px 10px -7px rgb(0 0 0 / 30%)",
-      });
-    } else {
-      // if (pathname === '/') {
-      $("#topbar").css({ background: "transparent", boxShadow: "unset" });
-      // } else {
-      //   $("#topbar").css({ background: "#171753", boxShadow: "unset" });
-      // }
+    if (pathname === "/") {
+      if ($(this).scrollTop() >= aTop) {
+        console.log("$(this).scrollTop()", $(this).scrollTop(), aTop);
+        $("#topbar").css({
+          background:
+            "linear-gradient(90.16deg, #030332 0.14%, #171753 99.87%)",
+          boxShadow: "0px 10px 10px -7px rgb(0 0 0 / 30%)",
+        });
+      } else {
+        $("#topbar").css({ background: "transparent", boxShadow: "unset" });
+      }
     }
-  }
   });
   $(document).ready(function () {
     var pathname = window.location.pathname;
-    if (pathname === '/') {
+    if (pathname === "/") {
       $("#topbar").css({ background: "transparent" });
     }
+
+    $(document).click(function (event) {
+      var clickover = $(event.target);
+      var _opened = $(".navbar-collapse").hasClass("show");
+      if (_opened === true && !clickover.hasClass("in-nav")) {
+          $("button.navbar-toggler").click();
+      }
+  });
   });
 })(jQuery); // End of use strict
