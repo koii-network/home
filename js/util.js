18de832f36692ee7149122701df26ea312acc176
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-02-24 14:05:52 +0800

    fix issues

--- js/util.js
+++ js/util.js
@@ -9,11 +9,27 @@
 
   $(document.body).scroll(function () {
     var aTop = $("#topbar").height();
+    var pathname = window.location.pathname;
+    if (pathname === '/') {
     if ($(this).scrollTop() >= aTop) {
       console.log("$(this).scrollTop()", $(this).scrollTop(), aTop);
-      $("#topbar").css({ background: 'linear-gradient(90.16deg, #030332 0.14%, #171753 99.87%)', boxShadow: "0px 10px 10px -7px rgb(0 0 0 / 30%)" });
+      $("#topbar").css({
+        background: "linear-gradient(90.16deg, #030332 0.14%, #171753 99.87%)",
+        boxShadow: "0px 10px 10px -7px rgb(0 0 0 / 30%)",
+      });
     } else {
-      $("#topbar").css({ background: 'transparent', boxShadow: "unset" });
+      // if (pathname === '/') {
+      $("#topbar").css({ background: "transparent", boxShadow: "unset" });
+      // } else {
+      //   $("#topbar").css({ background: "#171753", boxShadow: "unset" });
+      // }
+    }
+  }
+  });
+  $(document).ready(function () {
+    var pathname = window.location.pathname;
+    if (pathname === '/') {
+      $("#topbar").css({ background: "transparent" });
     }
   });
 })(jQuery); // End of use strict
