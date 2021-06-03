6e7f7e3cb692685dc9760fa1a48d0512c2bfef76
itsimpledeveloper917 <itsimpledeveloper917@gmail.com>
2021-06-03 15:30:12 -0400

    update nav utils

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,19 @@
 var Categories = ['learn', 'earn', 'connect', 'explore']
-function showSubMenu() {
+function showSubMenu(category, sm) {
   const subMenu = $("#sub_menu")
+  subMenu.find('.sub-menu-container').css('display', 'none')
+  subMenu.find('.sub-menu-container.sub-'+category).css('display', 'flex')
   if(subMenu.hasClass('active')) $("#sub_menu").removeClass('active')
   else $("#sub_menu").addClass('active')
 }
 function openSubMenu (category, subMenu) {
   console.log(category)
   console.log(subMenu)
-  showSubMenu()
+  var pathname = window.location.pathname;
+  if(pathname.includes('/' + category) || category === 'learn' || category === 'earn'){
+    console.log({pathname})
+    showSubMenu(category, subMenu)
+  }else{
+    location.href = '/' + category + '/' + subMenu
+  }
 }
