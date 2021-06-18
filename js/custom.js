11ff3de449b68659d818f1f8c3d982ae0cf885a5
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-06-18 13:37:18 -0400

    update join us

--- js/custom.js
+++ js/custom.js
@@ -160,5 +160,18 @@ $(document).ready(function() {
       return ;
     }
   })
-  
+  var win = $(this); //this = window
+    if (win.width() >= 572) { 
+      $(".btn-subscription :first-child").html("Join the movement")
+    }else{
+      $(".btn-subscription :first-child").html("Join us")
+    }
+  $(window).on('resize', function(){
+    var win = $(this); //this = window
+    if (win.width() >= 572) { 
+      $(".btn-subscription :first-child").html("Join the movement")
+    }else{
+      $(".btn-subscription :first-child").html("Join us")
+    }
+  });
 })
