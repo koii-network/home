c6a7ddca13385e8fd840771df793976177643ae0
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-07-11 22:42:27 -0400

    lazy loading image

--- js/custom.js
+++ js/custom.js
@@ -96,6 +96,7 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 $(document).ready(function() {
+  $("img.lazy").lazyload(); // image lazy loading
   var pathname = window.location.pathname;
   var paths = pathname.split('/')
   console.log({paths})
