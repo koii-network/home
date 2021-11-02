24100503faabbfd0a20e2a9dd8e8d5b7d0b803df
李东樾 <soma@koii.network>
2021-11-02 15:03:34 -0300

    improve function and style

--- js/jobs-fwdl.js
+++ js/jobs-fwdl.js
@@ -1,6 +1,15 @@
 function option1() {
+  var elements = document.getElementsByClassName('btn btn-secondary active');
+    while(elements.length > 0){
+        elements[0].classList.remove('active');
+    }
+  var active = document.getElementById("fedl");
+  active.classList.add("active");
+
   document.getElementById("detail").innerHTML = `
+  <div class="bcak-icon"><img src="/img/jobs/icon/Job/Return-button.svg" onclick="jobmenu()"></div>
   <div class="job-content">
+
   <div class="job-title">Front-End Web Development Lead</div>
   <div class="job-green">Who we are</div>
   <div class="job-detail">
