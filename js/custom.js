07880b3c84393e6ee4fbe642a8f4dd6dcf248823
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-11-18 13:07:58 -0500

    vertical slider testing

--- js/custom.js
+++ js/custom.js
@@ -228,7 +228,16 @@ function nextFwCurrent() {
     $(".cbp-fwnext-c").css("display", "none");
   }
 }
-
+var offset = 0;
+var count = $(".slide-item-wrapper > * > *").length;
+window.setInterval(
+  function() {
+    offset = (offset - 104) % (count * 104); // 104px div height (incl margin)
+    $(".slide-item-wrapper > *").css({
+      "transform": "translateY(" + offset + "px)",
+    });
+  }, 3000);
+  
 (jQuery)(function($) {
   var pathname = window.location.pathname;
   var paths = pathname.split('/')
