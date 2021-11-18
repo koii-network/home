36f0dfcc78f3f5b4c1bd304d0d9144ecb1c79386
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2021-11-17 21:02:36 -0500

    update slider

--- js/custom.js
+++ js/custom.js
@@ -211,9 +211,77 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-function goto(id) {
-  
-}
+/*
+  custom pure slider
+*/
+function Slider( element ) {
+	this.el = document.querySelector( element );
+	this.init();
+}	
+
+Slider.prototype = {
+	init: function() {
+		this.links = this.el.querySelectorAll( "#slider-nav a" );
+		this.wrapper = this.el.querySelector( "#slider-wrapper" );
+		this.navigate();
+	},
+	navigate: function() {
+	
+		for( var i = 0; i < this.links.length; ++i ) {
+			var link = this.links[i];
+			this.slide( link );	
+		}
+    // this.setCurrentLink(this.links[1])
+	},
+	
+	animate: function( slide ) {
+		var parent = slide.parentNode;
+		var caption = slide.querySelector( ".caption" );
+		var captions = parent.querySelectorAll( ".caption" );
+		for( var k = 0; k < captions.length; ++k ) {
+			var cap = captions[k];
+			if( cap !== caption ) {
+				cap.classList.remove( "visible" );
+			}
+		}
+		caption.classList.add( "visible" );	
+	},
+	
+	slide: function( element ) {
+		var self = this;
+		element.addEventListener( "click", function( e ) {
+			e.preventDefault();
+			var a = this;
+			self.setCurrentLink( a );
+      console.log( a )
+			var index = parseInt( a.getAttribute( "data-slide" ), 10 ) + 1;
+			var currentSlide = self.el.querySelector( ".slide:nth-child(" + index + ")" );
+			self.wrapper.style.left = "-" + currentSlide.offsetLeft + "px";
+			self.animate( currentSlide );
+		}, false);
+	},
+	setCurrentLink: function( link ) {
+		var parent = link.parentNode;
+		var a = parent.querySelectorAll( "a" );
+		
+		link.className = "current";
+		
+		for( var j = 0; j < a.length; ++j ) {
+			var cur = a[j];
+			if( cur !== link ) {
+				cur.className = "";
+			}
+		}
+	}	
+};
+
+document.addEventListener( "DOMContentLoaded", function() {
+	var aSlider = new Slider( "#slider_custom" );
+  $("#key2").trigger( "click");
+});
+/*
+  end custom pure slider
+*/
 
 (jQuery)(function($) {
   var pathname = window.location.pathname;
