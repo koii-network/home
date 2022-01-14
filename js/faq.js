5eeed2fe87aea9ef781778b331270d0a16e3db8b
itsimpledeveloper917 <itsimpledeveloper@gmail.com>
2022-01-14 08:23:33 +0800

    update search form

--- js/faq.js
+++ js/faq.js
@@ -1,40 +1,50 @@
 
 window.addEventListener('load', (event) => {
-    $("#cta-general").click(function() {
+    $("#cta-general").click(function () {
         $('html, body').animate({
             scrollTop: eval($('#general').offset().top - 74)
         }, 1000);
     });
-    $("#cta-finnie").click(function() {
+    $("#cta-finnie").click(function () {
         $('html, body').animate({
             scrollTop: eval($('#finnie').offset().top - 74)
         }, 1000);
     });
-    $("#cta-nodes").click(function() {
+    $("#cta-nodes").click(function () {
         $('html, body').animate({
             scrollTop: eval($('#nodes').offset().top - 74)
         }, 1000);
     });
-    $("#cta-build").click(function() {
+    $("#cta-build").click(function () {
         $('html, body').animate({
             scrollTop: eval($('#build').offset().top - 74)
         }, 1000);
     });
-    $("#cta-invest").click(function() {
+    $("#cta-invest").click(function () {
         $('html, body').animate({
             scrollTop: eval($('#invest').offset().top - 74)
         }, 1000);
     });
-    $("#cta-token").click(function() {
+    $("#cta-token").click(function () {
         $('html, body').animate({
             scrollTop: eval($('#token').offset().top - 74)
         }, 1000);
     });
-    $("#cta-partnerships").click(function() {
+    $("#cta-partnerships").click(function () {
         $('html, body').animate({
             scrollTop: eval($('#partnerships').offset().top - 74)
         }, 1000);
     });
+    $("#search-input").submit(function (event) {
+        event.preventDefault()
+        var $inputs = $('#search-input :input');
+        var values = {};
+        $inputs.each(function () {
+            values[this.name] = $(this).val();
+        });
+        var inputValue = values.faq
+        console.log({ inputValue })
+    })
 });
 var qaList = [
     {
